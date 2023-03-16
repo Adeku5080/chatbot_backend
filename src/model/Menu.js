3f7ec864d6130540cc8df3f81b58c808adb84406
Ali Adeku <aliadeku.aam@gmail.com>
2023-03-16 15:52:03 +0100

    Create Menu model

---
+++ src/model/Menu.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const MenuSchema = new mongoose.Schema({
+    name :{
+        type:String
+    },
+
+    price: {
+     type:Number
+    }
+})
+
+module.exports = mongoose.model("Menu",MenuSchema)
