8a1082f252884f70b5e9e5cdb8c29f6a14d9fd59
Ali Adeku <aliadeku.aam@gmail.com>
2023-03-16 15:56:03 +0100

    create menu controller functions

---
+++ src/controllers/menu.js
@@ -0,0 +1,29 @@
+const Menu = require("../model/Menu");
+
+const createItem = async (req, res) => {
+  res.send("hello");
+};
+
+const getAllItems = async (req, res) => {
+  res.send("hello");
+};
+
+const getItem = async (req, res) => {
+  res.send("hello");
+};
+
+const deleteItem = async (req, res) => {
+  res.send("hello");
+};
+
+const updateItem = async (req, res) => {
+  res.send("hello");
+};
+
+module.exports = {
+  createItem,
+  getAllItems,
+  getItem,
+  deleteItem,
+  updateItem,
+};
--- src/routers/Menu.js
+++ src/routers/Menu.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const menuRouter = express.Router();
+const {createItem,getAllItems,getItem,deleteItem,updateItem} = require("../controllers/menu")
 
 menuRouter.route("/").get(getAllItems).post(createItem);
 menuRouter.route("/:id").get(getItem).patch(updateItem).delete(deleteItem);
